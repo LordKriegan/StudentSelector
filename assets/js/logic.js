f321a8ed9a7fc918613b059e11021fd77b0d8f33
Qamar Stationwala <YoPappy0@gmail.com>
2018-09-19 03:57:33 -0700

    changed html onclicks to jquery event handlers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,27 +1,33 @@
 let list = listOrig.slice();
-getRandomInt = (min, max) => {
+
+const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-pickStudent = () => {
-    const picked = list[getRandomInt(0, list.length - 1)];
-    $("#studentName").text(picked);
-}
-removeStud = (id) => {
-    list.splice(id, 1);
-    buildList();
-}
-buildList = () => {
+
+const buildList = () => {
     $("#studentList").empty();
     list.forEach((elem, id) => {
-        let newNameElem = $("<span class='studentListItm'><button class=' btn btn-danger' onclick='removeStud(" + id + ")'>x</button> " + elem + "</span>");
+        let newNameElem = $("<span class='studentListItm'><button class='remStud btn btn-danger' data-id='" + id + "'>x</button> " + elem + "</span>");
+        console.log($(newNameElem));
         $("#studentList").append(newNameElem)
     })
 }
 
-resetList = () => {
-    list = listOrig.slice();
-    buildList();
-}
 window.onload = () => {
     buildList();
+
+    $("#resetBtn").on("click", () => {
+        list = listOrig.slice();
+        buildList();
+    })
+
+    $("#pickStudentBtn").on("click", () => {
+        const picked = list[getRandomInt(0, list.length - 1)];
+        $("#studentName").text(picked);
+    })
+
+    $(".remStud").on("click", () => {
+        list.splice($(this).attr("data-id"), 1);
+        buildList();
+    })
 }
